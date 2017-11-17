c6e4a77a0fb28bdfd012157a60c9e3f8bcb4ffd8
minimallinux <anthonymcgowan1@gmail.com>
2017-11-17 00:24:34 +0000

    Correct jshint error

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
-window.onscroll = function() {scrollFunction()};
-  	function scrollFunction(){
-		/*Change timer to suit page size*/
+ function scrollFunction(){
   	    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
   	        document.querySelector("#topBtn").style.display = "block";
   	    } else {
   	        document.querySelector("#topBtn").style.display = "none";
   	    }
+  window.onscroll = function(){scrollFunction();};   
 
   	}
-  	function topFunction() {
+  function topFunction() {
       	document.body.scrollTop = 0;
       	document.documentElement.scrollTop = 0;
   	}
-
